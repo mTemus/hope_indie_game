aa4b1d314a2ce69db378bf0d7a249ed156235c73
Temus <temus@wir.pl>
2021-02-27 17:54:21 +0100

    code cleanup

--- Assets/Code/System/Properties/GlobalProperties.cs
+++ Assets/Code/System/Properties/GlobalProperties.cs
@@ -1,11 +1,12 @@
 
+using UnityEngine;
+
 namespace Code.System.Properties
 {
     public static class GlobalProperties
     {
         private static int tileSize = 2;
-
-
+        
         public static int TileSize => tileSize;
     }
 }
