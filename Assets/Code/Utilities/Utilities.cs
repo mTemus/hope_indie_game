7f5915e49de9721ad25e98ccb3eaaebbc1a01dfe
Temus <temus@wir.pl>
2021-02-27 18:00:08 +0100

    add method to increment idx

--- Assets/Code/Utilities/Utilities.cs
+++ Assets/Code/Utilities/Utilities.cs
@@ -7,5 +7,15 @@ namespace Code.Utilities
         //TODO: fix main cam
         public static Vector2 GetMouseWorldPosition2D() =>
             Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        public static int IncrementIdx(int idx, int value, int maxValue)
+        {
+            if (idx + value >= maxValue) 
+                return 0;
+            if(idx + value < 0) 
+                return maxValue;
+            
+            return idx + value;
+        }
     }
 }
