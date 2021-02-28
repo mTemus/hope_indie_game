0bc5a4a47478bec0635fe3639da39fbe772e1a37
Temus <temus@wir.pl>
2021-02-28 22:20:02 +0100

    add input state transition

--- Assets/Code/Player/Tools/ToolHammer.cs
+++ Assets/Code/Player/Tools/ToolHammer.cs
@@ -1,10 +1,13 @@
+using Code.System;
+using Code.System.PlayerInput;
+
 namespace Code.Player.Tools
 {
     public class ToolHammer : Tool
     {
         public override void UseTool()
         {
-            
+            Managers.Instance.Input.SetState(InputManager.BuildingSelectingInputState);
         }
     }
 }
