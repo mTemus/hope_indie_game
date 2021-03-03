47d5e54c2021b9a5e3ed5856e42ed83a1384566e
Temus <temus@wir.pl>
2021-03-03 12:40:00 +0100

    change building only to village area

--- Assets/Code/Player/Tools/ToolHammer.cs
+++ Assets/Code/Player/Tools/ToolHammer.cs
@@ -1,5 +1,7 @@
 using Code.System;
+using Code.System.Area;
 using Code.System.PlayerInput;
+using UnityEngine;
 
 namespace Code.Player.Tools
 {
@@ -7,6 +9,11 @@ namespace Code.Player.Tools
     {
         public override void UseTool()
         {
+            if (Managers.Instance.Areas.GetPlayerArea().Type != AreaType.VILLAGE) {
+                Debug.LogError("Can't build outside village!");
+                return;
+            }
+            
             Managers.Instance.Input.SetState(InputManager.BuildingSelectingInputState);
         }
     }
