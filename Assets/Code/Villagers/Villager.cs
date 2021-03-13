30baba221e509566ffb608782d081922503131df
Temus <temus@wir.pl>
2021-03-13 21:16:20 +0100

    update movement methods

--- Assets/Code/Villagers/Villager.cs
+++ Assets/Code/Villagers/Villager.cs
@@ -15,5 +15,13 @@ namespace Code.Villagers
             transform.position = Vector3.MoveTowards(transform.position , position, speed * Time.deltaTime);
         }
         
+        public void MoveTo(Vector3 position, float villagerSpeed)
+        {
+            transform.position = Vector3.MoveTowards(transform.position , position, villagerSpeed * Time.deltaTime);
+        }
+
+        public bool IsOnPosition(Vector3 position) =>
+            transform.position == position;
+
     }
 }
