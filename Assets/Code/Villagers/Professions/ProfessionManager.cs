91b4f580e7aa8e965148ea212d7f7dcb5d4ebcb5
Temus <temus@wir.pl>
2021-03-15 13:00:33 +0100

    move code to tasks manager

--- Assets/Code/Villagers/Professions/ProfessionManager.cs
+++ Assets/Code/Villagers/Professions/ProfessionManager.cs
@@ -9,56 +9,12 @@ namespace Code.Villagers.Professions
 {
     public class ProfessionManager : MonoBehaviour
     {
-        private List<BuildingTask> buildingTasks = new List<BuildingTask>();
-        private List<ResourceCarryingTask> resourceCarryingTasks = new List<ResourceCarryingTask>();
-        
         private List<VillagerBuilder> builders = new List<VillagerBuilder>();
-
+       
         public void HireBuilder(GameObject villager)
         {
             VillagerBuilder builder = villager.AddComponent<VillagerBuilder>();
             
-        }
-
-        private Task GetTaskByProfession(ProfessionType professionType)
-        {
-            Task taskToGet = null;
-            
-            switch (professionType) {
-                case ProfessionType.UNEMPLOYED:
-                    break;
-                
-                case ProfessionType.BUILDER:
-                    taskToGet = buildingTasks.FirstOrDefault(task => task.ResourcesDelivered) ?? (Task) resourceCarryingTasks.FirstOrDefault(task =>
-                        task.Profession == ProfessionType.BUILDER);
-                    break;
-                
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(professionType), professionType, null);
-            }
-
-            return taskToGet;
-        }
-        
-        public Task GetTask(ProfessionType profession)
-        {
-            Task taskToGet = null;
-            
-            switch (profession) {
-                case ProfessionType.UNEMPLOYED:
-                    break;
-                
-                case ProfessionType.BUILDER:
-                    taskToGet = buildingTasks.FirstOrDefault(task => task.ResourcesDelivered) ?? 
-                                (Task) resourceCarryingTasks.FirstOrDefault(task =>
-                        task.Profession == ProfessionType.BUILDER);
-                    break;
-                
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(profession), profession, null);
-            }
-
-            return taskToGet;
         }
     }
 }
