2baaa49334054c1caaf36e17f5a34757de18a028
Temus <temus@wir.pl>
2021-03-25 11:59:34 +0100

    Implement tool action for displaying villager properties

--- Assets/Code/Player/Tools/PlayerToolVillagersBook.cs
+++ Assets/Code/Player/Tools/PlayerToolVillagersBook.cs
@@ -1,4 +1,6 @@
-using UnityEngine;
+using Code.System;
+using Code.System.PlayerInput;
+using Code.Villagers.Entity;
 
 namespace Code.Player.Tools
 {
@@ -6,7 +8,14 @@ namespace Code.Player.Tools
     {
         public override void UseTool()
         {
-            Debug.Log("NOT IMPLEMENTED --- Villager Book");
+            //TODO: stopVillager to move while getting properties
+
+            Villager villager = Managers.Instance.Player.Player.VillagerToInteract;
+
+            if (villager == null) return;
+            villager.Profession.enabled = false;
+            Managers.Instance.GUI.VillagerPropertiesPanel.OpenPropertiesPanel(villager);
+            Managers.Instance.Input.SetState(InputManager.VillagerPropertiesInputState);
         }
     }
 }
