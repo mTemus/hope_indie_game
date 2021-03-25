20cc1a6defda60777a513e76698cad26502db72a
Temus <temus@wir.pl>
2021-03-25 11:50:42 +0100

    Update gui manager and its end points

--- Assets/Code/GUI/GUIManager.cs
+++ Assets/Code/GUI/GUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Code.GUI.BuildingSelecting;
 using Code.GUI.PlayerToolsMenu;
+using Code.GUI.Villagers;
 using Code.Resources;
 using UnityEngine;
 
@@ -8,16 +9,25 @@ namespace Code.GUI
 {
     public class GUIManager : MonoBehaviour
     {
+        [Header("Player GUI")]
         [SerializeField] private RadialToolsMenu playerToolsMenu;
         [SerializeField] private BuildingSelectingMenu buildingSelectingMenu;
 
+        [Header("Villager GUI")] 
+        [SerializeField] private VillagerPropertiesPanel villagerPropertiesPanel;
+        
+        [Header("Other")]
         [SerializeField] private List<Sprite> resourceIcons;
+
         public Sprite GetResourceIcon(ResourceType resourceType)
         {
             //TODO: this should be changed to addressables!
             return resourceIcons.Find(sprite => sprite.name == resourceType.ToString().ToLower());
         }
-        
+
+
+        public VillagerPropertiesPanel VillagerPropertiesPanel => villagerPropertiesPanel;
+
         public BuildingSelectingMenu BuildingSelectingMenu => buildingSelectingMenu;
 
         public RadialToolsMenu PlayerToolsMenu => playerToolsMenu;
