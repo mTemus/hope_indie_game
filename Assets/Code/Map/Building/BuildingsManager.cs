f6d481a880fda3b46b629dbba2280a2a884fc53d
Temus <temus@wir.pl>
2021-03-27 08:46:46 +0100

    Add buildings manager as a endpoint for finding specific building

--- Assets/Code/Map/Building/BuildingsManager.cs
+++ Assets/Code/Map/Building/BuildingsManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Code.Map.Building.Buildings.Components;
 using Code.System;
 using UnityEngine;
@@ -6,6 +9,18 @@ namespace Code.Map.Building
 {
     public class BuildingsManager : MonoBehaviour
     {
+        private readonly List<KeyValuePair<Type, Building>> buildings = new List<KeyValuePair<Type, Building>>();
+        
+        public void AddBuilding(Building b)
+        {
+            buildings.Add(new KeyValuePair<Type, Building>(b.GetType(), b));
+        }
+
+        public Building[] GetAllBuildingsOfType(Type buildingType)
+        {
+            return (from buildingKVP in buildings where buildingKVP.Key == buildingType select buildingKVP.Value).ToArray();
+        }
+        
         public Warehouse GetClosestWarehouse() =>
             Managers.Instance.Areas.GetVillageArea().GetWarehouse();
         
