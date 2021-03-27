7bc45fb51eb3ac19d53db4a72a1da64a9a82268c
Temus <temus@wir.pl>
2021-03-27 12:06:31 +0100

    Update giving profession to a vilager system

--- Assets/Code/Villagers/Professions/ProfessionManager.cs
+++ Assets/Code/Villagers/Professions/ProfessionManager.cs
@@ -1,20 +1,77 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using Code.Villagers.Entity;
 using Code.Villagers.Professions.Types;
-using Code.Villagers.Tasks;
 using UnityEngine;
 
 namespace Code.Villagers.Professions
 {
     public class ProfessionManager : MonoBehaviour
     {
-        private List<VillagerBuilder> builders = new List<VillagerBuilder>();
-       
-        public void HireBuilder(GameObject villager)
+        private readonly List<Villager> unemployed = new List<Villager>();
+        private readonly List<Villager> builders = new List<Villager>();
+        private readonly List<Villager> lumberjacks = new List<Villager>();
+
+        private void FireVillagerFromOldProfession(Villager villager)
         {
-            VillagerBuilder builder = villager.AddComponent<VillagerBuilder>();
+            switch (villager.Profession.Type) {
+                case ProfessionType.Unemployed:
+                    unemployed.Remove(villager);
+                    break;
+                
+                case ProfessionType.Builder:
+                    builders.Remove(villager);
+                    break;
+                
+                case ProfessionType.Lumberjack:
+                    lumberjacks.Remove(villager);
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
             
+            DestroyImmediate(villager.GetComponent(typeof(Profession)));
+        }
+        
+        private void MakeVillagerUnemployed(Villager villager)
+        {
+            villager.gameObject.AddComponent<VillagerUnemployed>();
+            unemployed.Add(villager);
+        }
+        
+        private void HireBuilder(Villager villager)
+        {
+            villager.gameObject.AddComponent<VillagerBuilder>();
+            builders.Add(villager);
+        }
+
+        private void HireLumberjack(Villager villager)
+        {
+            villager.gameObject.AddComponent<VillagerLumberjack>();
+            lumberjacks.Add(villager);
+        }
+        
+        public void SetVillagerProfession(Villager villager, ProfessionType professionType)
+        {
+            FireVillagerFromOldProfession(villager);
+
+            switch (professionType) {
+                case ProfessionType.Unemployed:
+                    MakeVillagerUnemployed(villager);
+                    break;
+                
+                case ProfessionType.Builder:
+                    HireBuilder(villager);
+                    break;
+                
+                case ProfessionType.Lumberjack:
+                    HireLumberjack(villager);
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(professionType), professionType, null);
+            }
         }
     }
 }
