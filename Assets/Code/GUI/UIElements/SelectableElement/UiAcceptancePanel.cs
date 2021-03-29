9e05d2621f354b208ee39525da36ee6eb981364d
Temus <temus@wir.pl>
2021-03-29 17:27:26 +0200

    Update acceptable panel first element

--- Assets/Code/GUI/UIElements/SelectableElement/UiAcceptancePanel.cs
+++ Assets/Code/GUI/UIElements/SelectableElement/UiAcceptancePanel.cs
@@ -2,6 +2,10 @@ namespace Code.GUI.UIElements.SelectableElement
 {
     public class UiAcceptancePanel : UiSelectablePanel
     {
-        
+        private void Awake()
+        {
+            currentElement = elementsToSelect[0];
+            currentElement.OnElementSelected();
+        }
     }
 }
