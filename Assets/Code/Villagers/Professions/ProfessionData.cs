1b31d32bd9ca5b7d7c7714c3d9cb1fcd857f027e
Temus <temus@wir.pl>
2021-04-05 12:36:43 +0200

    Update workplace

--- Assets/Code/Villagers/Professions/ProfessionData.cs
+++ Assets/Code/Villagers/Professions/ProfessionData.cs
@@ -8,15 +8,20 @@ namespace Code.Villagers.Professions
     [CreateAssetMenu(fileName = "Profession Data", menuName = "Game Data/Villagers/Profession Data", order = 0)]
     public class ProfessionData : ScriptableObject
     {
+        [Header("Profession data")]
         [SerializeField] private ProfessionType professionType;
-        [SerializeField] private Transform workspace;
         [SerializeField] private VillagersStatistics requiredStats;
         [SerializeField] private int goldPerDay;
 
+        [Header("Workplace data")] 
+        [SerializeField] private Transform workplace;
+        
         public void SetGoldPerDay(int amount) =>
             goldPerDay = amount;
 
-        public Type WorkplaceType => workspace.GetComponent<Building>().GetType();
+        public Type WorkplaceType => workplace.GetComponent<Building>().GetType();
+        
+        public BuildingType WorkplaceBuildingType => workplace.GetComponent<Building>().BuildingType;
         
         public ProfessionType ProfessionType => professionType;
 
