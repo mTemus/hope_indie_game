7332e3e3e79f7c2d73020bd6373a1047ba71bf4f
Temus <temus@wir.pl>
2021-04-05 18:21:00 +0200

    Add method for getting closest building to a position

--- Assets/Code/Map/Building/BuildingsManager.cs
+++ Assets/Code/Map/Building/BuildingsManager.cs
@@ -13,8 +13,6 @@ namespace Code.Map.Building
     
     public class BuildingsManager : MonoBehaviour
     {
-        private readonly List<KeyValuePair<Type, Building>> buildings = new List<KeyValuePair<Type, Building>>();
-
         private readonly List<Building> resourcesBuildings = new List<Building>();
         private readonly List<Building> villageBuildings = new List<Building>();
         private readonly List<Building> industryBuildings = new List<Building>();
@@ -62,6 +60,22 @@ namespace Code.Map.Building
                 .Where(building => building.GetType() == classType)
                 .Where(building => building.CanBeOccupied())
                 .ToArray();
+        }
+
+        public Building GetClosestBuildingOfClass(BuildingType buildingType, Type classType, Vector3 position)
+        {
+            List<Building> buildings = new List<Building>(GetAllBuildingOfClass(buildingType, classType));
+            Building closestBuilding = buildings[0];
+            float bestDistance = Vector3.Distance(closestBuilding.transform.position, position);
+
+            for (int i = 1; i < buildings.Count; i++) {
+                float distance = Vector3.Distance(buildings[i].transform.position, position);
+
+                if (distance > bestDistance) continue;
+                bestDistance = distance;
+                closestBuilding = buildings[i];
+            }
+            return closestBuilding;
         }
     }
 }
