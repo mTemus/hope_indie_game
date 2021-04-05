1703c0876b1c2c1ba540a2ab8a778660a1cd8938
Temus <temus@wir.pl>
2021-04-05 12:31:47 +0200

    Add builders guild for builders profession instead of warehouse

---
+++ Assets/Code/Map/Building/Buildings/Components/Resources/BuildersGuild.cs
@@ -0,0 +1,17 @@
+namespace Code.Map.Building.Buildings.Components.Resources
+{
+    public class BuildersGuild : Building
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
