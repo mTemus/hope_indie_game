e5a24fd867764648bedbe04584f9554578e025a3
Temus <temus@wir.pl>
2021-04-09 17:58:09 +0200

    Update manager for reserving resources, for example for building tasks

--- Assets/Code/Resources/ResourcesManager.cs
+++ Assets/Code/Resources/ResourcesManager.cs
@@ -7,40 +7,55 @@ namespace Code.Resources
 {
    public class ResourcesManager : MonoBehaviour
    {
-      private static Resource _wood = new Resource(ResourceType.WOOD, 300);
-      private static Resource _stone = new Resource(ResourceType.STONE, 300);
+      private static readonly Resource Wood = new Resource(ResourceType.WOOD, 300);
+      private static readonly Resource Stone = new Resource(ResourceType.STONE, 300);
+
+      private readonly Dictionary<Task, Resource> reservedResources = new Dictionary<Task, Resource>();
       
-      private Resource GetResourceByType(ResourceType resourceType)
+      private int maxStorage = 100;
+      
+      private bool CanWithdraw(Resource resource, int amount) =>
+         resource.amount - amount >= 0;
+
+      public Resource GetResourceByType(ResourceType resourceType)
       {
          return resourceType switch {
-            ResourceType.WOOD => _wood,
-            ResourceType.STONE => _stone,
+            ResourceType.WOOD => Wood,
+            ResourceType.STONE => Stone,
             _ => throw new ArgumentOutOfRangeException(nameof(resourceType), resourceType, null)
          };
       }
-
-      private bool CanWithdraw(Resource resource, int amount) =>
-         resource.amount - amount >= 0;
 
       public void StoreResource(ResourceType resource, int amount)
       {
          GetResourceByType(resource).amount += amount;
       }
 
-      public Resource GetResourceAmount(ResourceType resource, int amount)
+      public void IncreaseStorage(int value)
       {
-         Resource takenResource = new Resource(resource);
-         Resource resourceToWithdraw = GetResourceByType(resource);
-
-         if (!CanWithdraw(resourceToWithdraw, amount)) {
-            takenResource.amount = 0;
-            return takenResource;
-         }
-
-         takenResource.amount = amount;
-         resourceToWithdraw.amount -= amount;
-         return takenResource;
+         maxStorage += value;
+      }
+      
+      public void ReserveResources(Task tKey, Resource resource)
+      {
+         Resource current = GetResourceByType(resource.Type);
+         current.amount -= resource.amount;
+         reservedResources[tKey] = new Resource(resource);
+         Debug.LogWarning("Reserved: " + reservedResources[tKey].Type + " for: " + tKey.GetType());
       }
 
+      public Resource WithdrawReservedResource(Task tKey)
+      {
+         Resource withdrawn = reservedResources[tKey];
+         return withdrawn;
+      }
+
+      public void ClearReservedResource(Task tKey)
+      {
+         Debug.LogWarning("Cleared: " + reservedResources[tKey].Type + " for: " + tKey.GetType());
+         reservedResources.Remove(tKey);
+      }
+      
+      public int MAXStorage => maxStorage;
    }
 }
