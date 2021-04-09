699bbf591b49ffb6dab135e3bc4e6507740de426
Temus <temus@wir.pl>
2021-04-09 17:47:03 +0200

    Reworked task for new tasks storing mechanism

--- Assets/Code/Villagers/Tasks/ResourceCarryingTask.cs
+++ Assets/Code/Villagers/Tasks/ResourceCarryingTask.cs
@@ -1,9 +1,9 @@
 using System;
+using Code.Map.Building;
 using Code.Map.Building.Buildings.Components.Resources;
 using Code.Resources;
+using Code.System;
 using Code.System.Properties;
-using Code.Villagers.Entity;
-using Code.Villagers.Professions;
 using UnityEngine;
 
 namespace Code.Villagers.Tasks
@@ -24,14 +24,29 @@ namespace Code.Villagers.Tasks
         private readonly Resource requiredResource;
         private ResourceCarryingTaskState resourceCarryingState = ResourceCarryingTaskState.GO_TO_STORAGE;
 
-        private readonly Action<Resource> onResourceDelivered;
+        private readonly bool reservedResources;
         
-        public ResourceCarryingTask(int taskPriority,
-            ProfessionType professionType,
-            Vector3 taskPosition, 
-            Warehouse storage, 
-            Resource requiredResource, 
-            Action<Resource> onResourceDelivered)
+        private Building fromStorage;
+        private Vector3 fromStoragePosition;
+
+        private readonly Func<ResourceType, int, Resource> onResourceWithdraw;
+        private readonly Func<Task, int, Resource> onReservedResourceWithdraw;
+        private readonly Action<Resource> onResourceDelivery;
+
+        public ResourceCarryingTask(Resource requiredResource, Building toStorage, Action<Resource> onResourceDelivery, Func<ResourceType, int, Resource> onResourceWithdraw, Building fromStorage = null)
+        {
+            this.requiredResource = requiredResource;
+            this.fromStorage = fromStorage;
+            this.onResourceDelivery = onResourceDelivery;
+            this.onResourceWithdraw = onResourceWithdraw;
+
+            reservedResources = false;
+            taskPosition = toStorage.PivotedPosition;
+
+            if (fromStorage != null) fromStoragePosition = fromStorage.PivotedPosition;
+        }
+        
+        public ResourceCarryingTask(Resource requiredResource, Building toStorage, Action<Resource> onResourceDelivery, Func<Task, int, Resource> onReservedResourceWithdraw, Building fromStorage = null)
         {
             base.TaskPriority = taskPriority;
             base.TaskPosition = taskPosition;
@@ -64,10 +79,25 @@ namespace Code.Villagers.Tasks
                     break;
                 
                 case ResourceCarryingTaskState.TAKE_RESOURCES:
-                    Worker.Profession.CarriedResource = storage.GetResourceFromStorage(requiredResource.Type, requiredResource.amount > GlobalProperties.MAXResourceHeld ? 
-                        GlobalProperties.MAXResourceHeld : requiredResource.amount);
-                    Worker.UI.SetResourceIcon(true, requiredResource.Type);
-                    requiredResource.amount -= Worker.Profession.CarriedResource.amount;
+                    // Worker.Profession.CarriedResource = storage.GetReservedResource(this, requiredResource.amount > GlobalProperties.MAXResourceHeld ? 
+                    //     GlobalProperties.MAXResourceHeld : requiredResource.amount);
+
+                    if (reservedResources) {
+                        worker.Profession.CarriedResource = onReservedResourceWithdraw.Invoke(this,
+                            requiredResource.amount > GlobalProperties.MAXResourceHeld
+                                ? GlobalProperties.MAXResourceHeld
+                                : requiredResource.amount);
+                    }
+                    else {
+                        worker.Profession.CarriedResource = onResourceWithdraw.Invoke(
+                            requiredResource.Type,
+                            requiredResource.amount > GlobalProperties.MAXResourceHeld
+                                ? GlobalProperties.MAXResourceHeld
+                                : requiredResource.amount);
+                    }
+                    
+                    worker.UI.SetResourceIcon(true, requiredResource.Type);
+                    requiredResource.amount -= worker.Profession.CarriedResource.amount;
                     resourceCarryingState = ResourceCarryingTaskState.GO_ON_TASK_POSITION;
                     break;
                 
