3dbf7200f15ad00894dc80367390e5254327aebc
Temus <temus@wir.pl>
2021-04-20 19:13:08 +0200

    Add inheritance from workplace

--- Assets/Code/Map/Building/Buildings/Types/Village/TownHall.cs
+++ Assets/Code/Map/Building/Buildings/Types/Village/TownHall.cs
@@ -1,7 +1,39 @@
+using Code.Villagers.Professions;
+using Code.Villagers.Tasks;
+using NotImplementedException = System.NotImplementedException;
+
 namespace Code.Map.Building.Buildings.Types.Village
 {
-    public class TownHall : Building
+    public class TownHall : Workplace
     {
-        
+        protected override Task GetNormalTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task GetResourceCarryingTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddTaskToDo(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetAutomatedTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TakeTaskBackFromWorker(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void FireNormalWorker(Profession worker)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
