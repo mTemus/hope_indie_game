595a309d9f8d483afe6c730738782f61beefdff2
Temus <temus@wir.pl>
2021-04-22 20:48:11 +0200

    Update storing buildingData in building.cs and building prefab instead of setting it through code for no reason

--- Assets/Code/Map/Building/Building.cs
+++ Assets/Code/Map/Building/Building.cs
@@ -5,21 +5,19 @@ namespace Code.Map.Building
 {
     public abstract class Building : MonoBehaviour
     {
+        [Header("Building Data")]
+        [SerializeField] private BuildingData data;
+        
+        [Header("Building Modules")]
         [SerializeField] private BuildingStorageModule storage;
-        
-        private BuildingData data;
-        
+
         public Vector3 PivotedPosition =>
             transform.position + data.EntrancePivot;
 
         public Vector3 PivotedLocalPosition =>
             transform.localPosition + data.EntrancePivot;
 
-        public BuildingData Data
-        {
-            get => data;
-            set => data = value;
-        }
+        public BuildingData Data => data;
 
         public BuildingStorageModule Storage => storage;
     }
--- Assets/Code/System/Initialization/InitializeBuilding.cs
+++ Assets/Code/System/Initialization/InitializeBuilding.cs
@@ -5,15 +5,12 @@ namespace Code.System.Initialization
 {
     public class InitializeBuilding : InitializeObject
     {
-        [SerializeField] private BuildingData buildingData;
-        
         public override void InitializeMe()
         {
             Building building = GetComponent<Building>();
-            building.Data = buildingData;
             Area.Area myArea = Managers.Instance.Areas.GetAreaByCoords(Vector3Int.FloorToInt(transform.position));
-            myArea.AddBuilding(building, buildingData);
-            Managers.Instance.Buildings.AddBuilding(buildingData.BuildingType, building);
+            myArea.AddBuilding(building, building.Data);
+            Managers.Instance.Buildings.AddBuilding(building.Data.BuildingType, building);
         }
     }
 }
