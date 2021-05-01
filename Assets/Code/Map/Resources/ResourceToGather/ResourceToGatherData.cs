fd78949c2114349caed4e19a2d82e6912c5659a0
Temus <temus@wir.pl>
2021-05-01 12:02:54 +0200

    Add areas on which resource can occur, so worker will be able to find it

--- Assets/Code/Map/Resources/ResourceToGather/ResourceToGatherData.cs
+++ Assets/Code/Map/Resources/ResourceToGather/ResourceToGatherData.cs
@@ -1,3 +1,4 @@
+using Code.System.Areas;
 using UnityEngine;
 
 namespace Code.Map.Resources.ResourceToGather
@@ -5,11 +6,16 @@ namespace Code.Map.Resources.ResourceToGather
     [CreateAssetMenu(fileName = "Resource to gather data", menuName = "Game Data/Map Objects/Resource to Gather Data", order = 0)]
     public class ResourceToGatherData : ScriptableObject
     {
+        [Header("Resource properties")]
         [SerializeField] private ResourceType resourceType;
         [SerializeField] private ResourceToGatherType resourceToGatherType;
         [SerializeField] private int amount;
         [SerializeField] private int maximumGatherers;
 
+        [Header("World data")] 
+        [SerializeField] private AreaType[] occurAreas;
+
+        public AreaType[] OccurAreas => occurAreas;
         public ResourceType ResourceType => resourceType;
         public int Amount => amount;
         public int MaximumGatherers => maximumGatherers;
