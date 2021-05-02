8abdb59314e94e3e928375114bdc62349e016f12
Temus <temus@wir.pl>
2021-05-02 13:29:00 +0200

    Add workplace type property for description what is workplace used for

--- Assets/Code/Map/Building/WorkplaceProperties.cs
+++ Assets/Code/Map/Building/WorkplaceProperties.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 namespace Code.Map.Building
 {
+    public enum WorkplaceType
+    {
+        Gathering, Production, Services
+    }
+    
     [CreateAssetMenu(fileName = "Workplace Properties", menuName = "Game Data/Map Objects/Workplace Properties", order = 1)]
     public class WorkplaceProperties : ScriptableObject
     {
@@ -10,9 +15,10 @@ namespace Code.Map.Building
         [Header("Workplace properties")]
         [SerializeField] private int workers;
         [SerializeField] private int haulers;
+        [SerializeField] private WorkplaceType type;
         
         public int Workers => workers;
-
         public int Haulers => haulers;
+        public WorkplaceType Type => type;
     }
 }
