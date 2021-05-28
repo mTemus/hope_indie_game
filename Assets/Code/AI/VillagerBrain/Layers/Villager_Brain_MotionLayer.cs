79ba3fac131a5c77cf6142abef00e90f6a45a34a
Temus <temus@wir.pl>
2021-05-28 12:06:20 +0200

    Move behaviour from villager to Villager_Brain_MotionLayer

--- Assets/Code/AI/VillagerBrain/Layers/Villager_Brain_MotionLayer.cs
+++ Assets/Code/AI/VillagerBrain/Layers/Villager_Brain_MotionLayer.cs
@@ -1,14 +1,36 @@
-using Code.Villagers.Entity;
+using UnityEngine;
 
 namespace Code.AI.VillagerBrain.Layers
 {
     public class Villager_Brain_MotionLayer : BrainLayer
     {
-        private Villager villager;
-
+        [SerializeField] private float speed = 5f;
+        
         public override void Initialize(Villager_Brain villagerBrain)
         {
             
         }
+        
+        
+        public bool MoveTo(Vector3 position)
+        {
+            Vector3 villagerPosition = transform.position;
+            villagerPosition = Vector3.MoveTowards(villagerPosition , position, speed * Time.deltaTime);
+            
+            transform.position = villagerPosition;
+
+            return villagerPosition == position;
+        }
+        
+        public bool MoveTo(Vector3 position, float villagerSpeed)
+        {
+            Vector3 villagerPosition = transform.position;
+            villagerPosition = Vector3.MoveTowards(villagerPosition , position, villagerSpeed * Time.deltaTime);
+            
+            transform.position = villagerPosition;
+
+            return villagerPosition == position;
+        }
+        
     }
 }
