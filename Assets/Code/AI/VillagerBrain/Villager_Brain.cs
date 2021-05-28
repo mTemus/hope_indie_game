b3a073298e70645627014bbbd8f3a498016137d8
Temus <temus@wir.pl>
2021-05-28 18:48:25 +0200

    Add field for work layer

--- Assets/Code/AI/VillagerBrain/Villager_Brain.cs
+++ Assets/Code/AI/VillagerBrain/Villager_Brain.cs
@@ -9,16 +9,30 @@ namespace Code.AI.VillagerBrain
         [SerializeField] private Villager_Brain_PerceptionLayer perception;
         [SerializeField] private Villager_Brain_BehaviourLayer behaviour;
         [SerializeField] private Villager_Brain_MotionLayer motion;
+        [SerializeField] private Villager_Brain_WorkLayer work;
     
         public Villager_Brain_PerceptionLayer Perception => perception;
         public Villager_Brain_BehaviourLayer Behaviour => behaviour;
         public Villager_Brain_MotionLayer Motion => motion;
+        public Villager_Brain_WorkLayer Work => work;
 
         private void Awake()
         {
             perception.Initialize(this);
             behaviour.Initialize(this);
             motion.Initialize(this);
+        }
+
+        private void Update()
+        {
+            perception.Update();
+        }
+
+        public void ClearBehaviourAIComponents()
+        {
+            behaviour.BehaviourTree.StopBehaviour();
+            behaviour.BehaviourTree.blackboard.variables.Clear();
+            behaviour.BehaviourTree.graph = null;
         }
     }
 }
