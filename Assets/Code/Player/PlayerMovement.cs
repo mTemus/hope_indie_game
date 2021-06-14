a058f3254a6a5bb0cdce7cd9fcd9df693eaab97c
Temus <temus@wir.pl>
2021-06-14 09:04:19 +0200

    Update player character facing direction

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -5,9 +5,30 @@ namespace Code.Player
     public class PlayerMovement : MonoBehaviour
     {
         public float speed = 10f;
-
-        public void Move(Vector3 direction) =>
-            transform.position += direction * (Time.deltaTime * speed);
+        private bool facingRight = true;
         
+        public void Move(Vector3 direction)
+        {
+            transform.position += direction * (Time.deltaTime * speed);
+
+            if (direction == Vector3.left) {
+                if (!facingRight) return;
+                Flip();
+            }
+            else {
+                if (facingRight) return;
+                Flip();
+            }
+        }
+        
+        private void Flip()
+        {
+            facingRight = !facingRight;
+
+            var myTransform = transform;
+            Vector3 theScale = myTransform.localScale;
+            theScale.x *= -1;
+            myTransform.localScale = theScale;
+        }
     }
 }
